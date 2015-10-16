aa9aa36bc8b31de612f03d30f47d7e8c511c8194
Fernando Pacheco <fernando.pacheco@ingesur.com.uy>
2015-10-16 03:46:49 -0300

    Se pasa a publica las cargas de archivos

--- src/trash-prob/trashWithMovingDumps/integration/shared/loadfromfiles.h
+++ src/trash-prob/trashWithMovingDumps/integration/shared/loadfromfiles.h
@@ -15,17 +15,16 @@ class LoadFromFiles {
     otherloc_t *getOtherlocs(unsigned int &otherloc_count);
     vehicle_t *getVehicles(unsigned int &vehicle_count);
     ttime_t *getTtimes(unsigned int &ttime_count);
-
-  private:
     void load_containers( std::string infile );
     void load_otherlocs( std::string infile );
     void load_vehicles( std::string infile );
     void load_ttimes( std::string infile );
+
+  private:
     container_t parseContainer( std::string line );
     otherloc_t parseOtherloc( std::string line );
     vehicle_t parseVehicle( std::string line );
     ttime_t parseTtime( std::string line );
-
     std::vector<container_t> mContainers;
     std::vector<otherloc_t> mOtherLocs;
     std::vector<vehicle_t> mVehicles;
